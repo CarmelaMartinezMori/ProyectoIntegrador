2708e5aa81a7ec3cab3c31f420254d6e2f66b803
CarmelaMartinezMori <cmartinezmori@udesa.edu.ar>
2021-06-15 15:56:48 -0300

    resultados

--- js/resultados.js
+++ js/resultados.js
@@ -13,8 +13,21 @@ window.addEventListener(`load`, function(){
 
   let queryString = new URLSearchParams (location.search)  
   let buscar = queryString.get ("searchBar");
-  console.log(buscar);
 
+  let error = document.querySelector(".error");
+
+  if (buscar === ""){
+    error.innerHTML += `<h4 id="margen"> Ingrese su búsqueda </h4>`;
+    resultados.classList.add ("resultados");
+  } else if (buscar.length <= 3){
+    error.innerHTML += `<h4 id="margen"> Por favor ingrese más caractéres para realizar la búsqueda </h4>`;
+    resultados.classList.add ("resultados");
+  } else {
+    error.innerHTML+= `<h4> Término buscado: ${buscar} </h4>`;
+    error.style.margin="50px 0 0 0";
+  }
+
+  let error2 = document.getElementsByClassName("error2")
   let albums = document.getElementById("albums");
   fetch (`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/album?q=${buscar}&key=${apiKey}`)
     .then (response =>{
@@ -22,14 +35,19 @@ window.addEventListener(`load`, function(){
     })
     .then(infoAlbum=>{
       let resultados = infoAlbum.data;
-      for (let i=0; i<5; i++)
-      albums.innerHTML+= `<li>
+        if (resultados == null){
+          error2.innerHTML += `<h3> No se encontraron resultados en Álbumes </h3>`;
+        } else{for (let i=0; i<resultados.length; i++){
+          albums.innerHTML+= `<li>
             <a href="albums.html?id=${resultados[i].id}">
+              
                 <img src= ${resultados[i].cover_xl} alt="Album Cover Not Available">
                 <h4> ${resultados[i].title}</h4>
                 <p> Álbum • ${resultados[i].artist.name} </p>
             </a>
         </li>`
+        }
+      }
     })
     .catch (error =>{
       console.log(error);
@@ -40,16 +58,19 @@ window.addEventListener(`load`, function(){
     return response.json()
   })
     .then (infoArtista =>{
-    let resultadosArtsitas = infoArtista.data;
-    for(let i=0;i<5;i++){
-    artistas.innerHTML +=
-    `<li>
-        <a href="artistas.html?id=${resultadosArtsitas[i].id}">
-            <img src= ${resultadosArtsitas[i].picture_big}>
-            <h4> ${resultadosArtsitas[i].name}</h4>
-        </a>
-    </li>
-    `}
+    let resultadosArtistas = infoArtista.data;
+    if (resultadosArtistas == null){
+      error2.innerHTML += `<h3> No se encontraron resultados en Artistas </h3>`;
+    } else{
+      for(let i=0;i<5;i++){
+        artistas.innerHTML +=
+          `<li>
+            <a href="artistas.html?id=${resultadosArtistas[i].id}">
+              <img src= ${resultadosArtistas[i].picture_big}>
+              <h4> ${resultadosArtistas[i].name}</h4>
+            </a>
+          </li>`}
+    }
   })
   .catch (error =>{
     console.log(error);
@@ -63,18 +84,21 @@ window.addEventListener(`load`, function(){
     .then (infoTrack =>{
     let resultadosTracks = infoTrack.data;
     console.log(resultadosTracks);
-    for(let i=0;i<5;i++){
-    tracks.innerHTML +=
-    `<li>
-        <a href="canciones.html?id=${resultadosTracks[i].id}">
-            <img src= ${resultadosTracks[i].album.cover_xl}>
-            <h4> ${resultadosTracks[i].title}</h4>
-        </a>
-    </li>
-    `}
+    if (resultadosTracks == null){
+      error2.innerHTML += `<h3> No se encontraron resultados en Canciones </h3>`;
+    }else{
+      for(let i=0;i<5;i++){
+        tracks.innerHTML +=
+          `<li>
+            <a href="canciones.html?id=${resultadosTracks[i].id}">
+              <img src= ${resultadosTracks[i].album.cover_xl}>
+              <h4> ${resultadosTracks[i].title}</h4>
+          </a>
+      </li>`}
+    }
+    
   })
   .catch (error =>{
     console.log(error);
   })
-    
 })
