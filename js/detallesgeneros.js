49fd034552f2bce539bf1c6d6a0a50d74bdef9a9
delfinaaa <dboglione@udesa.edu.ar>
2021-06-22 17:29:43 -0300

    detallesgeneros

---
+++ js/detallesgeneros.js
@@ -0,0 +1,43 @@
+function goBack(){
+    window.history.back();
+};
+function goForward(){
+    window.history.forward();
+}
+window.addEventListener("load", function(){
+
+    let apiKey = `6ee4ba95329b7c4561d268ce9d34aca3`;
+    let artistas = document.querySelector("#artists");
+    let nombreDelGenero = document.querySelector("#nombreDelGenero");
+
+    let querystring=location.search
+    let queryString =new URLSearchParams(querystring)
+    let id = queryString.get("iddelgenero")
+
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${id}`)
+    .then(response =>{
+        return response.json();
+        
+    })
+    .then(data =>{
+        console.log(data)
+        nombreDelGenero.innerHTML += `Artistas de ${data.name}`;
+    })
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${id}/arists`)
+    .then(response =>{
+        return response.json();
+        
+    })
+    .then(infoArtista =>{
+        console.log(infoArtista)
+        for (let i=0;i<5;i++){
+            artistas.innerHTML+= `
+                <li>
+                    <a href= "artistas.html?id=${infoArtista.data[i].id}">
+                        <img src="${infoArtista.data[i].picture_xl}" alt="Foto no disponible">
+                        <h4> ${infoartista.data[i].name}</h4>
+                    </a>
+                </li>`
+        }   
+    })     
+})
